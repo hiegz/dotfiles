75f48c6b4c7fdd10077b5e9bf57eb74be7d74f6e
Volodymyr But <vbut.dev+git@gmail.com>
2024-02-22 11:23:59 +0100

    feat(swaybar): display battery info

--- .config/sway/scripts/status.sh
+++ .config/sway/scripts/status.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+#
+# Battery Info
+# ---
+
+charge_full=$(cat /sys/class/power_supply/BATT/charge_full)
+charge_now=$(cat /sys/class/power_supply/BATT/charge_now)
+
 modules=(
+	"Battery: $(($charge_now * 100 / $charge_full))%"
 	"$(date +"%a %F %H:%M")"
 )
 
 printf "%s" "${modules[0]}"
-# printf " | %s" "${modules[@]:1}"
+printf " | %s" "${modules[@]:1}"
