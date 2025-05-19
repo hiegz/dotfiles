2572bddd9c666456346a90620fe69868cd64af9b
Volodymyr But <git@hiegz.com>
2025-05-19 21:28:28 +0200

    make audiorelay settings persistent

--- .kickstart/obs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Creates a device where AudioRelay can stream audio into
-pactl load-module module-null-sink sink_name=audiorelay-virtual-mic-sink sink_properties=device.description=Virtual-Mic-Sink
-# Creates a device usable by other apps
-pactl load-module module-remap-source master=audiorelay-virtual-mic-sink.monitor source_name=audiorelay-virtual-mic-sink source_properties=device.description=Virtual-Mic
