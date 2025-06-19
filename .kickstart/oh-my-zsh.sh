9f949244e4b09c15932f86ed7e662819d166218f
Volodymyr But <git@hiegz.com>
2025-06-19 19:55:42 +0200

    oh-my-zsh: prevent shell change during setup

--- .kickstart/oh-my-zsh.sh
+++ .kickstart/oh-my-zsh.sh
@@ -4,4 +4,4 @@ paru -S --noconfirm --needed \
     sh                       \
     curl
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --skip-chsh --unattended
