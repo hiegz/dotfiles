26d6d8c71672893b96780282e8d634e3e78b13f6
Volodymyr But <git@hiegz.com>
2025-09-01 14:53:02 +0200

    actually don't

--- .kickstart/node.sh
+++ .kickstart/node.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-LATEST_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest |
-    grep -Po '"tag_name": "\K(.*)(?=")')
-PROFILE=/dev/null bash -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$LATEST_VERSION/install.sh | bash"
+if ! command -v node &> /dev/null; then
+    LATEST_VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest |
+        grep -Po '"tag_name": "\K(.*)(?=")')
+    PROFILE=/dev/null bash -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$LATEST_VERSION/install.sh | bash"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-nvm install node
+    nvm install node
+fi
 
-paru -S --noconfirm --needed \
+npm install -g \
+    typescript \
     typescript-language-server \
-    prettierd \
+    @fsouza/prettierd \
     prettier
 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
