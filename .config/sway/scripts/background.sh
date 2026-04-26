9b13deb4e2778eecebdb6831c869987daf70e497
Volodymyr But <git@hiegz.com>
2026-04-26 21:24:37 +0200

    move wallpaper to $HOME

--- .config/sway/scripts/background.sh
+++ .config/sway/scripts/background.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -f $HOME/Pictures/wallpaper.png ]; then
-	swaymsg output \* background "$HOME/Pictures/wallpaper.png" fill
+if [ -f $HOME/wallpaper.png ]; then
+	swaymsg output \* background "$HOME/wallpaper.png" fill
 else
 	swaymsg output \* background \#000000 solid_color
 fi
