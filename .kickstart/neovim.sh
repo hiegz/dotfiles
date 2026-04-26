7bfcfa05a65ab3fad4e84cc24eee0871e5779bf8
Volodymyr But <git@hiegz.com>
2026-04-26 21:39:43 +0200

    install tools used by neovim

--- .kickstart/neovim.sh
+++ .kickstart/neovim.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 paru -S --noconfirm --needed \
+    tree-sitter \
+    tree-sitter-c \
+    tree-sitter-bash \
+    tree-sitter-javascript \
+    tree-sitter-lua \
+    tree-sitter-markdown \
+    tree-sitter-python \
+    tree-sitter-query \
+    tree-sitter-rust \
+    tree-sitter-vim \
+    tree-sitter-vimdoc \
+    fd \
+    ripgrep \
     neovim-git
