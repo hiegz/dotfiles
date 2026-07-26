c6779fc42cf4c8f15a2f7870489d3d3849334268
Volodymyr But <git@hiegz.com>
2026-07-26 14:01:21 +0200

    minimize desktop package installer

--- .kickstart/desktop.sh
+++ .kickstart/desktop.sh
@@ -1,69 +1,44 @@
 #!/bin/bash
 
+packages=(
+    # Desktop
+    gnome
+
+    # Terminal
+    ghostty
+    less
+    fzf
+
+    # Applications
+    google-chrome
+    atril
+    calibre
+    ristretto
+    vlc
+    vlc-plugin-ffmpeg
+
+    # Themes
+    fluent-gtk-theme
+
+    # Icons
+    papirus-icon-theme
+
+    # Cursors
+    xcursor-vanilla-dmz
+    xcursor-vanilla-dmz-aa
+
+    # Fonts
+    ttf-ubuntu-mono-nerd
+    ttf-hack
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+)
+
 if ! command -v paru &>/dev/null; then
-    echo "sway installation error: paru is not installed"
+    echo "error: paru is not installed"
     echo "help: run 'make paru' in $HOME"
     exit 1
 fi
 
-paru -S --noconfirm --needed \
-    networkmanager \
-    network-manager-applet \
-    networkmanager-openvpn \
-    less \
-    htop \
-    wl-mirror \
-    wl-clipboard \
-    xorg-xwayland \
-    wayland \
-    pipewire \
-    pipewire-alsa \
-    pipewire-pulse \
-    pipewire-jack \
-    wireplumber \
-    xdg-desktop-portal-gtk \
-    xdg-desktop-portal-wlr \
-    xdg-desktop-portal \
-    noto-fonts \
-    noto-fonts-cjk \
-    noto-fonts-emoji \
-    ttf-ubuntu-mono-nerd \
-    ttf-hack \
-    qt5-base \
-    qt5-wayland \
-    qt6-base \
-    qt6-wayland \
-    gtk3 \
-    gtk4 \
-    materia-gtk-theme \
-    fluent-gtk-theme \
-    papirus-icon-theme \
-    xcursor-vanilla-dmz \
-    xcursor-vanilla-dmz-aa \
-    easyeffects \
-    pavucontrol \
-    lxappearance \
-    nwg-look \
-    brightnessctl \
-    gvfs \
-    sshfs \
-    thunar \
-    google-chrome \
-    atril \
-    calibre \
-    bemenu-wayland \
-    bemenu \
-    tmux \
-    fzf \
-    ristretto \
-    vlc \
-    vlc-plugin-ffmpeg \
-    ghostty \
-    dunst \
-    gtklock \
-    swaybg \
-    sway-contrib \
-    sway
-
-systemctl --user enable --now pipewire.service
-systemctl --user enable --now pipewire-pulse.socket
+paru -S --noconfirm --needed "${packages[@]}"
