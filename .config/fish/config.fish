8f6e8e538f2cd4916d6245bc3cff5a4860b61314
Volodymyr But <git@hiegz.com>
2026-08-12 12:35:48 +0200

    remove vim alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,6 @@ if status is-interactive
     config config --local status.showUntrackedFiles no
 
     alias tma "tmux attach-session; or tmux new-session -s hiegz -c ~ > /dev/null 2>&1"
-    alias vim "nvim"
 
     # FUCK BILL GATES IN HIS ASS
     alias fbgiha "find . -name '*:Zone.Identifier' -type f -delete"
